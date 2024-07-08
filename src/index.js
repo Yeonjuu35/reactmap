ded5c97bb6a4728184eccc3ce470b1f99dfba131
Yeonjuu35 <yeonjuu35@gmail.com>
2024-07-08 17:53:22 +0900

    map

--- src/index.js
+++ src/index.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-const myinfo = [{
-  price : 5000,
-  pronm : "신발",
-  protext : "팝니다"
-  },
-  {
-  price : 50000,
-  pronm : "신발",
-  protext : "팝니다.."
-  }
-];
+const myinfo =[{
+        price : 50000,
+        pronm : "신발",
+        protext : "싸요싸요!!! 언능 사가"
+    },
+    {
+        price : 100000,
+        pronm : "신발",
+        protext : "안싸요안싸요!!! 깍아줘"
+    },
+    {
+        price : 100000,
+        pronm : "신발아님",
+        protext : "안싸요안싸요!!! 깍아줘"
+    }];
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
+  // <>
+  //   {
+  //     myinfo.map((ele, idx)=>{
+  //       return(
+  //         <App sendinfo={myinfo[idx]}></App>
+  //       )
+  //     })
+  //   }
+  // </> 
   <>
-    <App sendinfo={myinfo[0]}></App>
-    <App sendinfo={myinfo[1]}></App>
-  </>
+    {
+      myinfo.map((ele, idx) => <App sendinfo={myinfo[idx]}></App>)
+      // 맵 안엔 리턴이 이미 존재, 간단하게 요약 가능
+    }
+  </> 
 );
